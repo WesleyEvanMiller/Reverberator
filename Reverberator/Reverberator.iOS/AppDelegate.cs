809179c60c46b3e3b5de0caa69cdc30e7e79f6b0
Wesley <wesmiller94@live.com>
2021-04-14 00:52:24 -0400

    Begin configuring iOS background tasks
    https://github.com/xamarin/ios-samples/blob/main/ios13/RefreshingAndMaintainingYourAppUsingBackgroundTasks/ColorFeed/AppDelegate.cs

--- Reverberator/Reverberator.iOS/AppDelegate.cs
+++ Reverberator/Reverberator.iOS/AppDelegate.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using BackgroundTasks;
 using Foundation;
@@ -20,16 +21,64 @@ namespace Reverberator.iOS
         //
         // You have 17 seconds to return from this method, or iOS will terminate your application.
         //
+        public static string AlertTaskId { get; } = "com.xamarin.Reverberator.alert";
+        public static NSString AlertSuccessNotificationName { get; } = new NSString($"{AlertTaskId}.success");
+
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
             LoadApplication(new App());
 
-            BGTaskScheduler.Shared.Register("test",null,null);
+            BGTaskScheduler.Shared.Register(AlertTaskId, null, task => HandleAppAlert(task as BGAppRefreshTask));
 
             Application thisapp = new Application();
 
             return base.FinishedLaunching(app, options);
         }
-    }
+
+		public override void DidEnterBackground(UIApplication application)
+		{
+			ScheduleAppRefresh();
+		}
+
+		#region Scheduling Tasks
+
+		void ScheduleAppRefresh()
+		{
+			NSNotificationCenter.DefaultCenter.AddObserver(AlertSuccessNotificationName, RefreshSuccess);
+
+			var request = new BGAppRefreshTaskRequest(AlertTaskId)
+			{
+				EarliestBeginDate = (NSDate)DateTime.Now.AddMinutes(15) // Fetch no earlier than 15 minutes from now
+			};
+
+			BGTaskScheduler.Shared.Submit(request, out NSError error);
+
+			if (error != null)
+				Debug.WriteLine($"Could not schedule app refresh: {error}");
+		}
+
+		#endregion
+
+		#region Handling Launch for Tasks
+
+		// Fetch the latest feed entries from server.
+		void HandleAppAlert(BGAppRefreshTask task)
+		{
+			ScheduleAppRefresh();
+
+			task.ExpirationHandler = () => operations.CancelOperations();
+
+			operations.FetchLatestPosts(task);
+		}
+
+		void RefreshSuccess(NSNotification notification)
+		{
+			NSNotificationCenter.DefaultCenter.RemoveObserver(AlertSuccessNotificationName);
+			var task = notification.Object as BGAppRefreshTask;
+			task?.SetTaskCompleted(true);
+		}
+
+		#endregion
+	}
 }
