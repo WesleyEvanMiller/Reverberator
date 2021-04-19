c81e619a3cc186919313adec9ad001ad016d7872
Wesley <wesmiller94@live.com>
2021-04-19 01:55:24 -0400

    Add initial configurations for Android hello notifications on background

---
+++ Reverberator/Reverberator.Android/ActivitySecond.cs
@@ -0,0 +1,36 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Reverberator.Droid
+{
+    [Activity(Label = "Activity1")]
+    public class ActivitySecond : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Get the count value passed to us from MainActivity:
+            var count = Intent.Extras.GetInt(MainActivity.COUNT_KEY, -1);
+
+            // No count was passed? Then just return.
+            if (count <= 0)
+            {
+                return;
+            }
+
+            // Display the count sent from the first activity:
+            SetContentView(Resource.Layout.Second);
+            var txtView = FindViewById<TextView>(Resource.Id.textView1);
+            txtView.Text = $"You clicked the button {count} times in the previous activity.";
+        }
+    }
+}
--- Reverberator/Reverberator.Android/MainActivity.cs
+++ Reverberator/Reverberator.Android/MainActivity.cs
@@ -4,25 +4,127 @@ using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using Android.Content;
+using System.Threading;
+using Android.Support.V4.App;
+using TaskStackBuilder = Android.Support.V4.App.TaskStackBuilder;
 
 namespace Reverberator.Droid
 {
-    [Activity(Label = "Reverberator", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "Reverberator", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        static readonly int NOTIFICATION_ID = 1000;
+        static readonly string CHANNEL_ID = "location_notification";
+        internal static readonly string COUNT_KEY = "count";
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+
+            CreateNotificationChannel();
+
+            var button = FindViewById<Button>(Resource.Id.MyButton);
+            button.Click += ButtonOnClick;
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        void CreateNotificationChannel()
+        {
+            if (Build.VERSION.SdkInt < BuildVersionCodes.O)
+            {
+                // Notification channels are new in API 26 (and not a part of the
+                // support library). There is no need to create a notification
+                // channel on older versions of Android.
+                return;
+            }
+
+            var name = Resources.GetString(Resource.String.channel_name);
+            var description = GetString(Resource.String.channel_description);
+            var channel = new NotificationChannel(CHANNEL_ID, name, NotificationImportance.Default)
+            {
+                Description = description
+            };
+
+            var notificationManager = (NotificationManager)GetSystemService(NotificationService);
+            notificationManager.CreateNotificationChannel(channel);
+        }
+
+        void ButtonOnClick(object sender, EventArgs eventArgs)
+        {
+            // Pass the current button press count value to the next activity:
+            var valuesForActivity = new Bundle();
+            valuesForActivity.PutInt(COUNT_KEY, count);
+
+            // When the user clicks the notification, SecondActivity will start up.
+            var resultIntent = new Intent(this, typeof(SecondActivity));
+
+            // Pass some values to SecondActivity:
+            resultIntent.PutExtras(valuesForActivity);
+
+            // Construct a back stack for cross-task navigation:
+            var stackBuilder = TaskStackBuilder.Create(this);
+            stackBuilder.AddParentStack(Class.FromType(typeof(SecondActivity)));
+            stackBuilder.AddNextIntent(resultIntent);
+
+            // Create the PendingIntent with the back stack:
+            var resultPendingIntent = stackBuilder.GetPendingIntent(0, (int)PendingIntentFlags.UpdateCurrent);
+
+            // Build the notification:
+            var builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                          .SetAutoCancel(true) // Dismiss the notification from the notification area when the user clicks on it
+                          .SetContentIntent(resultPendingIntent) // Start up this activity when the user clicks the intent.
+                          .SetContentTitle("Button Clicked") // Set the title
+                          .SetNumber(count) // Display the count in the Content Info
+                          .SetSmallIcon(Resource.Drawable.ic_stat_button_click) // This is the icon to display
+                          .SetContentText($"The button has been clicked {count} times."); // the message to display.
+
+            // Finally, publish the notification:
+            var notificationManager = NotificationManagerCompat.From(this);
+            notificationManager.Notify(NOTIFICATION_ID, builder.Build());
+
+            // Increment the button press count:
+            count++;
+        }
+
+
+    }
+
+    [Service(IsolatedProcess = true, Name = "com.xamarin.example.ReverbAlert")]
+    public class ReverbAlertService : Service
+    {
+        Timer timer;
+        public override void OnCreate()
+        {
+            base.OnCreate();
+        }
+
+        // Magical code that makes the service do wonderful things.
+        public override IBinder OnBind(Intent intent)
+        {
+            return null;
+        }
+
+        public override StartCommandResult OnStartCommand(Intent intent, StartCommandFlags flags, int startId)
+        {
+            //every 1 mins in miliseconds
+            timer = new Timer(HandleTimerCallback, 0, 0, 600000);
+            return base.OnStartCommand(intent, flags, startId);
+        }
+
+        private void HandleTimerCallback(object state)
+        {
+            //this part codes will run every 1 minutes
         }
     }
 }
